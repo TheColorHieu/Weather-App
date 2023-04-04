ec42cfae90ec6a4fd8001086474160dc3a76fe77
Hieu Tran <hieuthanhtran98@yahoo.com>
2023-04-04 11:27:01 -0700

    started the fetch function

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -4,4 +4,19 @@ const button = document.getElementById('output')
 button.addEventListener('click',function (event){
     event.preventDefault();
     console.log('testing')
+
 })
+
+//creating the fetch function 
+function fetchData(url){
+return fetch(url)
+.then(response => response.json())
+.then(data => {
+
+    return data;
+})
+.catch(error => console.error(error));
+
+}
+
+
